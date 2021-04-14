2d19e085e29957ee734c16b0e07f445530e0e960
EC2 Default User <ec2-user@ip-172-31-12-105.ec2.internal>
2021-04-14 04:54:27 +0000

    3章

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :homes
+  devise_for :users
+  # resources :homes
   
   root to: "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
